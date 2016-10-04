5d0e6321d5712b75891b07bf5303d64a8a5affcc
Timothy <b85205@gmail.com>
2016-10-04 11:47:40 -0400

    added a simple UML diagram

--- classes/classes.cpp
+++ classes/classes.cpp
@@ -1,5 +1,7 @@
 // classes.cpp : Defines the entry point for the console application.
-//
+//testing out the different syntax of classes, structs...
+//doing this as a refresh to c++ syntax
+
 
 #include "stdafx.h"
 
